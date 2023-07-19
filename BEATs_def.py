d7faaf09d57771554140c55ab3d32dc57ed6f0d9
lsl198651 <1986510281@qq.com>
2023-07-19 22:56:18 +0800

    解决def冲突

--- BEATs_def.py
+++ BEATs_def.py
@@ -17,17 +17,8 @@ def csv_reader_cl(file_name, clo_num):
     with open(file_name, encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
         column = [row[clo_num] for row in reader]
-
-def csv_reader_cl(file_name, clo_num):
-    with open(file_name, encoding='utf-8') as csvfile:
-        reader = csv.reader(csvfile)
-        column = [row[clo_num] for row in reader]
     return column
 
-
-def copy_wav(folder, idlist, mur, traintest):
-    for patient_id in idlist:
-        dir_path = folder + "\\" + mur + "\\" + patient_id
 
 def copy_wav(folder, idlist, mur, traintest):
     for patient_id in idlist:
@@ -47,7 +38,6 @@ def get_patientid(csv_path):
     with open(csv_path) as csvfile:
         reader = csv.reader(csvfile)
         id = [row[0] for row in reader]  # weight 同列的数据
-        id = [row[0] for row in reader]  # weight 同列的数据
     return id
 
 
@@ -58,40 +48,40 @@ def get_wav_data(dir_path, csv_path, Murmur: str, id_data, Murmur_locations):
     if not os.path.exists(csv_path):
         os.makedirs(csv_path)
 
-    for root,  dir,  file in os.walk(dir_path):
+    for root, dir, file in os.walk(dir_path):
         for subfile in file:
             wav_path = os.path.join(root, subfile)
             if os.path.exists(wav_path):
                 # 数据读取
-                print("reading: "+subfile)
+                print("reading: " + subfile)
                 y, sr = librosa.load(wav_path)
                 y_16k = librosa.resample(y=y, orig_sr=sr, target_sr=16000)
                 # print("y_16k size: "+y_16k.size)
-                if y_16k.shape[0]<3500:
-                    y_16k = np.pad(y_16k,(0,3500-y_16k.shape[0]),'constant',constant_values=(0,0))
-                elif  y_16k.shape[0]>3500:
-                    y_16k=y_16k[0:3500]                
+                if y_16k.shape[0] < 3500:
+                    y_16k = np.pad(y_16k, (0, 3500 - y_16k.shape[0]),
+                                   'constant',
+                                   constant_values=(0, 0))
+                elif y_16k.shape[0] > 3500:
+                    y_16k = y_16k[0:3500]
                 wav.append(y_16k)
                 # feature = pd.DataFrame(y_16k)
                 # save_path = csv_path +"\\"+ subfile+ ".csv"
                 # print("shape: "+feature.shape())
-                feature.to_csv(save_path, index=False, header=False)
+                # feature.to_csv(save_path, index=False, header=False)
 
                 # 标签读取
                 if Murmur == 'Absent':  # Absent
                     label.append(0)
-                else:                   # Present
-                # 先找到id对应的index,再通过索引找到murmur_locations和timing
-                    murmur_ap=subfile.split('_')[4]
-                    if murmur_ap == 'Absent':           # 说明该听诊区有杂音
-                        label.append(0)                 # 舒张期全部认为没有杂音
+                else:  # Present
+                    # 先找到id对应的index,再通过索引找到murmur_locations和timing
+                    murmur_ap = subfile.split('_')[4]
+                    if murmur_ap == 'Absent':  # 说明该听诊区有杂音
+                        label.append(0)  # 舒张期全部认为没有杂音
                     else:
-                        label.append(1)                     # 说明该听诊区无杂音
-    return np.array(wav),np.array(label)
+                        label.append(1)  # 说明该听诊区无杂音
+    return np.array(wav), np.array(label)
 
-def cal_len(dir_path, csv_path, Murmur: str, id_data, Murmur_locations):
-    slen = []
-    dlen = []
+
 def cal_len(dir_path, csv_path, Murmur: str, id_data, Murmur_locations):
     slen = []
     dlen = []
@@ -99,7 +89,7 @@ def cal_len(dir_path, csv_path, Murmur: str, id_data, Murmur_locations):
     if not os.path.exists(csv_path):
         os.makedirs(csv_path)
 
-    for root,  dir,  file in os.walk(dir_path):
+    for root, dir, file in os.walk(dir_path):
         for subfile in file:
             wav_path = os.path.join(root, subfile)
             wav_path = os.path.join(root, subfile)
@@ -122,19 +112,10 @@ def cal_len(dir_path, csv_path, Murmur: str, id_data, Murmur_locations):
                     dlen.append(waveform_16k.size)
     return np.array(slen), np.array(dlen)
 
-                    dlen.append(waveform_16k.size)
-    return np.array(slen), np.array(dlen)
-
 
 """
 读取csv文件返回feature和label
 """
-
-
-def get_mel_features(dir_path, absent_id, present_id):
-    feature_list = []
-    label_list = []
-    for root, dir, file in os.walk(dir_path):
 
 
 def get_mel_features(dir_path, absent_id, present_id):
@@ -147,15 +128,8 @@ def get_mel_features(dir_path, absent_id, present_id):
             csv_path = os.path.join(root, subfile)
             print("reading: " + subfile)
             # filename = r'E:\Shilong\murmur\LM_wav_dataset\train_csv\2530_AV_1.wav.csv'
-            df = pd.read_csv(csv_path,  header=None)
+            df = pd.read_csv(csv_path, header=None)
             data = np.array(df)
-            if data.shape[0] < 24:
-                data = np.pad(data, (0, 24 - data.shape[0]),
-                              'constant',
-                              constant_values=(0, 0))
-                data = data[:, 0:768]
-            elif data.shape[0] > 24:
-                data = data[0:24, :]
             if data.shape[0] < 24:
                 data = np.pad(data, (0, 24 - data.shape[0]),
                               'constant',
@@ -165,7 +139,6 @@ def get_mel_features(dir_path, absent_id, present_id):
                 data = data[0:24, :]
             feature_list.append(data)
             id = subfile.split('_')[0]
-            id = subfile.split('_')[0]
             if id in absent_id:
                 label_list.append(1)
             if id in present_id:
@@ -174,18 +147,12 @@ def get_mel_features(dir_path, absent_id, present_id):
     return np.array(feature_list), np.array(label_list)
 
 
-    return np.array(feature_list), np.array(label_list)
-
-
 # ========================/ dataset Class /========================== #
 class MyDataset(Dataset):
     """ my dataset."""
 
-class MyDataset(Dataset):
-    """ my dataset."""
-
     # Initialize your data, download, etc.
-    def __init__(self,  wavlabel,  wavdata):
+    def __init__(self, wavlabel, wavdata):
         # 直接传递data和label
         # self.len = wavlen
         self.data = torch.from_numpy(wavdata)
@@ -198,12 +165,8 @@ class MyDataset(Dataset):
         return dataitem.float(), labelitem.float()
 
     def __len__(self):
-
-    def __len__(self):
         # 返回文件数据的数目
         return len(self.data)
-
-
 
 
 # ========================/ logging init /========================== #
@@ -214,11 +177,7 @@ def logger_init(
     # 指定路径
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
-
-    date = datetime.now()
-    log_path = os.path.join(log_dir,
-                            str(date)[:13] + '-' + str(date.minute) + '.log')
-
+    # 指定日志格式
     date = datetime.now()
     log_path = os.path.join(log_dir,
                             str(date)[:13] + '-' + str(date.minute) + '.log')
@@ -226,12 +185,6 @@ def logger_init(
     logging.basicConfig(level=log_level,
                         format=formatter,
                         datefmt='%Y-%d-%m %H:%M:%S',
-                        handlers=[
-                            logging.FileHandler(log_path),
-                            logging.StreamHandler(sys.stdout)
-                        ])
-
-
                         handlers=[
                             logging.FileHandler(log_path),
                             logging.StreamHandler(sys.stdout)
@@ -247,12 +200,10 @@ class save_info(object):
         self.train_loss = train_loss
         self.test_acc = test_acc
         self.test_loss = test_loss
-        self.recall = recall
 
         writer.add_scalar("train_loss", train_loss, epoch)
         writer.add_scalar("test_loss", test_loss, epoch)
         writer.add_scalar("test_acc", test_acc, epoch)
-        writer.add_scalar("recall_score", recall)
 
         logging.info(f"epoch: " + str(self.epoch) + '/' + str(epoch_num))
         logging.info(f"train_loss: " + str('{:.3f}'.format(self.train_loss)))
