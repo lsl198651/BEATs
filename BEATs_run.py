6cb291b9c7f040284fa381cbf8b5630598e1f290
lsl198651 <1986510281@qq.com>
2023-09-06 14:06:47 +0800

    save4.1

---
+++ BEATs_run.py
@@ -0,0 +1,141 @@
+import argparse
+import torch
+import torch.profiler
+import logging
+import numpy as np
+from torch.utils.data.sampler import WeightedRandomSampler
+from datetime import datetime
+from torch import optim
+from transformers import optimization
+from torch.utils.data import DataLoader
+from BEATs import BEATs_Pre_Train_itere3
+from torch.utils.tensorboard import SummaryWriter
+from util.dataloaders import get_features
+from util.traintest import train_test
+from util.BEATs_def import (MyDataset, logger_init)
+
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--batch_size", type=int, default=64,
+                    help="args.batch_size for training")
+parser.add_argument("--learning_rate", type=float,
+                    default=0.0001, help="learning_rate for training")
+parser.add_argument("--num_epochs", type=int, default=300, help="num_epochs")
+parser.add_argument("--loss_type", type=str, default="CE",
+                    help="loss function", choices=["BCE", "CE"])
+parser.add_argument("--scheduler_flag", type=str, default=None,
+                    help="the dataset used", choices=["cos", "cos_warmup"],)
+parser.add_argument("--freqm_value",  type=int, default=0,
+                    help="frequency mask max length")
+parser.add_argument("--timem_value", type=int, default=10,
+                    help="time mask max length")
+parser.add_argument("--mask", type=bool, default=False,
+                    help="number of classes", choices=[True, False])
+parser.add_argument("--testset_balance", type=bool, default=False,
+                    help="balance absent and present in testset", choices=[True, False],)
+parser.add_argument("--Data_Augmentation", type=bool, default=False,
+                    help="Add data augmentation", choices=[True, False],)
+parser.add_argument("--grad_flag", type=bool, default=False,
+                    help="use grad_no_requiredn", choices=[True, False],)
+parser.add_argument("--samplerWeight", type=bool, default=False,
+                    help="use balanced sampler", choices=[True, False],)
+parser.add_argument("--layers", type=int, default=1, help="layers number")
+parser.add_argument("--train_total_model", type=bool, default=False,
+                    help="train total model", choices=[True, False],)
+parser.add_argument("--model", type=str,
+                    default="BEATs_iter3_plus_AS2M", help="the model used", choices=["BEATs_iter3_plus_AS2M", "BEATs_iter3_plus_AS20K", "BEATs_iter3", "BEATs_iter2"])
+parser.add_argument("--ap_ratio", type=float, default=1.0,
+                    help="ratio of absent and present")
+parser.add_argument("--confusion_matrix_path", type=float,
+                    default=1.0, help="ratio of absent and present",)
+parser.add_argument("--beta", type=float, default=(0.9, 0.98), help="beta")
+args = parser.parse_args()
+
+train_features, train_label, test_features, test_label = get_features(
+    args=args)
+# ========================/ setup loader /========================== #
+if args.samplerWeight == True:
+    weights = [3 if label == 1 else 1 for label in train_label]
+    Data_sampler = WeightedRandomSampler(
+        weights, num_samples=len(weights), replacement=True
+    )
+    train_loader = DataLoader(MyDataset(wavlabel=train_label, wavdata=train_features),
+                              sampler=Data_sampler, batch_size=args.args.batch_size, drop_last=True,)
+else:
+    train_loader = DataLoader(MyDataset(wavlabel=train_label, wavdata=train_features),
+                              batch_size=args.batch_size, drop_last=True, shuffle=True, pin_memory=True,)
+
+val_loader = DataLoader(
+    MyDataset(wavlabel=test_label, wavdata=test_features),
+    batch_size=args.batch_size, shuffle=True, drop_last=True, pin_memory=True,
+)
+
+# ========================/ dataset size /========================== #
+train_present_size = np.sum(train_label == 1)
+train_absent_size = np.sum(train_label == 0)
+test_present_size = np.sum(test_label == 1)
+test_absent_size = np.sum(test_label == 0)
+trainset_size = train_label.shape[0]
+testset_size = test_label.shape[0]
+
+# ========================/ setup padding /========================== #
+padding_size = train_features.shape[1]  # 3500
+padding = torch.zeros(
+    args.batch_size, padding_size
+).bool()  # we randomly mask 75% of the input patches,
+padding_mask = torch.Tensor(padding)
+
+MyModel = BEATs_Pre_Train_itere3(args=args)
+
+# ========================/ setup optimizer /========================== #
+if args.train_total_model == False:
+    for param in MyModel.BEATs.parameters():
+        param.requires_grad = False
+    optimizer = torch.optim.AdamW(filter(lambda p: p.requires_grad, MyModel.parameters()),
+                                  lr=args.learning_rate, betas=args.beta,)
+else:
+    optimizer = torch.optim.AdamW(MyModel.parameters(),
+                                  lr=args.learning_rate, betas=args.beta,)
+
+# ========================/ setup scaler /========================== #
+logger_init()
+logging.info(f"<<< {args.model} - {args.layers} fc layer >>>")
+logging.info(f"# Batch_size = {args.batch_size}")
+logging.info(f"# Num_epochs = {args.num_epochs}")
+logging.info(f"# Learning_rate = {args.learning_rate:.6f}")
+logging.info(f"# lr_scheduler = {args.scheduler_flag}")
+logging.info(f"# Padding_size = {padding_size}")
+logging.info(f"# Loss_fn = {args.loss_type}")
+logging.info(f"# Data Augmentation = {args.Data_Augmentation}")
+logging.info(f"# Testset_balance = {args.testset_balance}")
+logging.info(f"# Masking = {args.mask}")
+if args.mask == True:
+    logging.info(f"# freqm_value = {args.freqm_value}")
+    logging.info(f"# timem_value = {args.timem_value}")
+logging.info(f"# wegiht sampler = {args.samplerWeight}")
+logging.info(f"# train total model = {args.train_total_model}")
+logging.info(f"# Train_a/p = {train_absent_size}/{train_present_size}")
+logging.info(f"# Test_a/p = {test_absent_size}/{test_present_size}")
+logging.info(f"# Trainset_size = {trainset_size}")
+logging.info(f"# Testset_size = {testset_size}")
+logging.info("# Optimizer = " + str(optimizer))
+logging.info("# Notes : ")
+logging.info("-------------------------------------")
+confusion_matrix_path = r"./confusion_matrix/" + str(
+    datetime.now().strftime("%Y-%m%d %H%M")
+)
+tb_writer = SummaryWriter(
+    r"./tensorboard/" + str(datetime.now().strftime("%Y-%m%d %H%M"))
+)
+
+
+train_test(
+    model=MyModel,
+    train_loader=train_loader,
+    test_loader=val_loader,
+    padding=padding_mask,
+    optimizer=optimizer,
+    args=args,
+    tb_writer=tb_writer,
+    matrix_path=confusion_matrix_path,
+)
---
+++ util/traintest.py
@@ -0,0 +1,162 @@
+from torch.cuda.amp import autocast, GradScaler
+import torch
+import torch.nn as nn
+from util.BEATs_def import draw_confusion_matrix
+from torch import optim
+from transformers import optimization
+import logging
+from sklearn.metrics import confusion_matrix
+
+
+def train_test(
+    model,
+    train_loader,
+    test_loader,
+    padding,
+    optimizer,
+    args=None,
+    tb_writer=None,
+    matrix_path=None,
+):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)  # 放到设备中
+    # train model
+    lr = []
+    train_acc_list = []
+    test_acc_list = []
+
+    # for amp
+    scaler = GradScaler()
+    warm_up_ratio = 0.1
+    total_steps = len(train_loader) * args.num_epochs
+
+    if args.scheduler_flag == "cos":
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(
+            optimizer, T_max=10, eta_min=0)
+    elif args.scheduler_flag == "cos_warmup":
+        scheduler = optimization.get_cosine_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=warm_up_ratio * total_steps,
+            num_training_steps=total_steps,
+        )
+    if args.loss_type == "BCE":
+        loss_fn = nn.BCEWithLogitsLoss()  # BCELoss+sigmoid
+    elif args.loss_type == "CE":
+        loss_fn = nn.CrossEntropyLoss()  # 内部会自动加上Softmax层
+
+    for epoch in range(args.num_epochs):
+
+        train_loss = 0
+        correct_t = 0
+        train_len = 0
+
+        model.train()
+        for data_t, label_t in train_loader:
+            data_t, label_t = data_t.to(device), label_t.to(device)
+            padding = padding.to(device)
+            optimizer.zero_grad()
+            # with autocast(device_type='cuda', dtype=torch.float16):# 这函数害人呀，慎用
+            predict_t = model(data_t, padding)
+            if isinstance(loss_fn, torch.nn.BCEWithLogitsLoss):
+                pred = torch.argmax(predict_t, dim=1)
+                loss = loss_fn(predict_t, label_t.float())
+            else:
+                loss_t = loss_fn(predict_t, label_t.long())
+
+            # scaler.scale(loss).backward()
+            # scaler.step(optimizer)
+            # scaler.update()
+            # flood = (loss-0.17).abs()+0.17 # 用于防止梯度爆炸
+            loss_t.backward()
+            optimizer.step()
+            train_loss += loss_t.item()
+            pred_t = predict_t.max(1, keepdim=True)[
+                1
+            ]  # get the index of the max log-probability
+            pred_t = torch.squeeze(pred_t)
+            target_t = label_t.type(torch.int64)
+            correct_t += pred_t.eq(target_t).sum().item()
+            train_len += len(label_t)
+
+        if args.scheduler_flag is not None:
+            scheduler.step()
+
+        # evaluate model
+        model.eval()
+        label = []
+        pred = []
+        test_loss = 0
+        correct_v = 0
+
+        with torch.no_grad():
+            for data_v, label_v in test_loader:
+                data_v, label_v, padding = (
+                    data_v.to(device),
+                    label_v.to(device),
+                    padding.to(device),
+                )
+                optimizer.zero_grad()
+                predict_v = model(data_v, padding)
+                # recall = recall_score(y_hat, y)
+                if isinstance(loss_fn, torch.nn.BCEWithLogitsLoss):
+                    pred_v = torch.argmax(predict_v, dim=1)
+                    loss = loss_fn(predict_v[:, 1], label_t.float())
+                else:
+                    loss_v = loss_fn(predict_v, label_t.long())
+                test_loss += loss_v.item()
+                pred_v = predict_v.max(1, keepdim=True)[
+                    1
+                ]  # get the index of the max log-probability
+                pred_v = torch.squeeze(pred_v)
+                target_v = label_v.type(torch.int64)
+                correct_v += pred_v.eq(target_v).sum().item()
+                pred.extend(pred_v.cpu().tolist())
+                label.extend(label_v.cpu().tolist())
+                # from sklearn.metrics import classification_report
+                # y_pred = [0, 1, 0, 0, 1]
+                # y_correct = [1, 1, 0, 1, 1]
+                # print(classification_report(y_correct, y_pred))
+
+        for group in optimizer.param_groups:
+            lr_now = group["lr"]
+        lr.append(lr_now)
+
+        val_len = len(label)
+
+        # 更新权值
+        test_loss /= val_len
+        train_loss /= train_len
+        train_acc = correct_t / train_len
+        test_acc = correct_v / val_len
+        # acc = accuracy_score(pred , target)
+
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        max_train_acc = max(train_acc_list)
+        max_test_acc = test_acc_list[train_acc_list.index(max_train_acc)]
+
+        tb_writer.add_scalar("train_acc", train_acc, epoch)
+        tb_writer.add_scalar("test_acc", test_acc, epoch)
+        tb_writer.add_scalar("train_loss", train_loss, epoch)
+        tb_writer.add_scalar("test_loss", test_loss, epoch)
+        tb_writer.add_scalar("learning_rate", lr_now, epoch)
+
+        # a=save_info(num_epochs, epoch, loss, test_acc, test_loss)
+        logging.info(f"epoch:  {epoch + 1}/{args.num_epochs}")
+        logging.info(f"learning_rate: {lr_now:.6f}")
+        logging.info(
+            f"train_acc: {train_acc:.3%} train_loss: {train_loss:.4f}")
+        logging.info(f"test_acc: {test_acc:.3%} test_loss:{test_loss:.4f}")
+        logging.info(f"max_train_acc:{max_train_acc:.3%}")
+        logging.info(f"max_test_acc: {max_test_acc:.3%}")
+        logging.info(f"max_lr: {max(lr):.6f} , min_lr: {min(lr):.6f}")
+        logging.info("======================================")
+        # 画混淆矩阵
+        draw_confusion_matrix(
+            label,
+            pred,
+            ["Absent", "Present"],
+            "epoch" + str(epoch + 1) + ",testacc: {:.3%}".format(test_acc),
+            pdf_save_path=matrix_path,
+            epoch=epoch + 1,
+        )
