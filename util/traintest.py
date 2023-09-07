73bf715d51feb8d9e940d1cdd820843be7a4b915
lsl198651 <1986510281@qq.com>
2023-09-07 18:30:00 +0800

    修复bug

--- util/traintest.py
+++ util/traintest.py
@@ -15,22 +15,22 @@ def train_test(
     train_loader,
     test_loader,
     padding,
-    epochs,
     optimizer,
     args=None,
 ):
-    
-    tb_writer = SummaryWriter(r"./tensorboard/" + str(datetime.now().strftime("%Y-%m%d %H%M")))
-    confusion_matrix_path = r"./confusion_matrix/" + str(datetime.now().strftime("%Y-%m%d %H%M"))
 
-    lr=[]
-    max_test_acc=[]
-    max_train_acc=[]
+    tb_writer = SummaryWriter(
+        r"./tensorboard/" + str(datetime.now().strftime("%Y-%m%d %H%M")))
+    confusion_matrix_path = r"./confusion_matrix/" + \
+        str(datetime.now().strftime("%Y-%m%d %H%M"))
+
+    lr = []
+    max_test_acc = []
+    max_train_acc = []
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = model.to(device)  # 放到设备中
 
-    
     # for amp
 # ============lr scheduler================
     # scaler = GradScaler()
@@ -38,7 +38,8 @@ def train_test(
     total_steps = len(train_loader) * args.num_epochs
 
     if args.scheduler_flag == "cos":
-        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10, eta_min=0)
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(
+            optimizer, T_max=10, eta_min=0)
     elif args.scheduler_flag == "cos_warmup":
         scheduler = optimization.get_cosine_schedule_with_warmup(
             optimizer,
@@ -52,15 +53,16 @@ def train_test(
         loss_fn = nn.CrossEntropyLoss()  # 内部会自动加上Softmax层
     model.train()
 # ============ training ================
-    for epoch_num in range(args.num_epochs):
+    for epochs in range(args.num_epochs):
         # train model
         model.train()
         train_loss = 0
         correct_t = 0
-        train_len_=0
+        train_len = 0
 
         for data_t, label_t in train_loader:
-            data_t, label_t ,padding= data_t.to(device), label_t.to(device), padding.to(device)
+            data_t, label_t, padding = data_t.to(
+                device), label_t.to(device), padding.to(device)
 
             # with autocast(device_type='cuda', dtype=torch.float16):# 这函数害人呀，慎用
             predict_t = model(data_t, padding)
@@ -81,7 +83,7 @@ def train_test(
             pred_t = predict_t.max(1, keepdim=True)[
                 1
             ]  # get the index of the max log-probability
-            train_len+=len(label_t)
+            train_len += len(label_t)
             correct_t += pred_t.eq(label_t.view_as(pred_t)).sum().item()
 
         if args.scheduler_flag is not None:
@@ -111,7 +113,7 @@ def train_test(
                 pred_v = predict_v.max(1, keepdim=True)[
                     1
                 ]  # get the index of the max log-probability
-                test_loss+=loss_v.item() 
+                test_loss += loss_v.item()
                 correct_v += pred_v.eq(label_v.view_as(pred_v)).sum().item()
                 pred.extend(pred_v.cpu().tolist())
                 label.extend(label_v.cpu().tolist())
@@ -130,10 +132,11 @@ def train_test(
         max_train_acc.append(train_acc)
         max_test_acc.append(test_acc)
         max_train_acc_value = max(max_train_acc)
-        max_test_acc_value = max_test_acc[max_train_acc.index(max_train_acc_value)]
+        max_test_acc_value = max_test_acc[max_train_acc.index(
+            max_train_acc_value)]
 
-        tb_writer.add_scalar("train_acc", train_acc , epochs)
-        tb_writer.add_scalar("test_acc", test_acc , epochs)
+        tb_writer.add_scalar("train_acc", train_acc, epochs)
+        tb_writer.add_scalar("test_acc", test_acc, epochs)
         tb_writer.add_scalar("train_loss", train_loss, epochs)
         tb_writer.add_scalar("test_loss", test_loss, epochs)
         tb_writer.add_scalar("learning_rate", lr_now, epochs)
@@ -141,7 +144,10 @@ def train_test(
         # a=save_info(num_epochs, epoch, loss, test_acc, test_loss)
         logging.info(f"epoch: {epochs + 1}/{args.num_epochs}")
         logging.info(f"learning_rate: {lr_now:.6f}")
-        logging.info(f"train_acc: {train_acc:.3%} train_loss: {train_loss:.4f}")
+        logging.info(
+            f"train_acc: {train_acc:.3%} train_loss: {train_loss:.4f}")
+        logging.info(
+            f"train_acc: {test_acc:.3%} train_loss: {test_loss:.4f}")
         logging.info(f"max_train_acc: {max_train_acc_value:.3%}")
         logging.info(f"max_test_acc: {max_test_acc_value:.3%}")
         logging.info(f"max_lr:{max(lr):.6f}, min_lr:{min(lr):.6f}")
