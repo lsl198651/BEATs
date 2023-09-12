dcb79adc60745258166840a4afce880ed92ce3d7
lsl198651 <1986510281@qq.com>
2023-09-12 21:21:53 +0800

    save

--- BEATs_run.py
+++ BEATs_run.py
@@ -16,7 +16,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument("--batch_size", type=int, default=128,
                     help="args.batch_size for training")
 parser.add_argument("--learning_rate", type=float,
-                    default=0.0001, help="learning_rate for training")
+                    default=0.000001, help="learning_rate for training")
 parser.add_argument("--num_epochs", type=int, default=200, help="num_epochs")
 parser.add_argument("--layers", type=int, default=1, help="layers number")
 parser.add_argument("--loss_type", type=str, default="CE",
