3f3bb3c041e3042f1627922176c43c0c3a584563
lsl198651 <1986510281@qq.com>
2023-12-03 10:08:05 +0800

    修改lr训练，看看loss

--- BEATs_run.py
+++ BEATs_run.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     parser.add_argument("--batch_size", type=int, default=512,
                         help="args.batch_size for training")
     parser.add_argument("--learning_rate", type=float,
-                        default=0.0005, help="learning_rate for training")
+                        default=0.0001, help="learning_rate for training")
     parser.add_argument("--num_epochs", type=int,
                         default=100, help="num_epochs")
     parser.add_argument("--layers", type=int, default=3, help="layers number")
@@ -51,8 +51,8 @@ if __name__ == '__main__':
     parser.add_argument("--beta", type=float, default=(0.9, 0.98), help="beta")
     parser.add_argument("--cross_evalue", type=bool, default=False)
     parser.add_argument("--train_fold", type=list,
-                        default=['0', '1', '2', '3'])
-    parser.add_argument("--test_fold", type=list, default=['4'])
+                        default=['0', '1', '3', '4'])
+    parser.add_argument("--test_fold", type=list, default=['2'])
     parser.add_argument("--setType", type=str, default=r"\08_dropabsent")
     args = parser.parse_args()
     # 检测分折重复
