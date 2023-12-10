5a16b3bbae725adee88e4018b7d3b83b11d28186
lsl198651 <1986510281@qq.com>
2023-12-10 15:24:50 +0800

    save CNN+LSTM

--- model/CNNModel.py
+++ model/CNNModel.py
@@ -64,11 +64,11 @@ class AudioClassifier(nn.Module):
         self.ap = nn.AdaptiveAvgPool2d(output_size=1)
 
         # wide features
-        self.wide = nn.Linear(in_features=6, out_features=20)
-        self.lin = nn.Linear(in_features=168, out_features=2)
+        self.wide = nn.Linear(in_features=6, out_features=10)
+        self.lin = nn.Linear(in_features=64, out_features=2)
         self.lin1 = nn.Linear(in_features=80, out_features=128)
-        self.segLSTM = nn.LSTM(
-            input_size=84, hidden_size=84, num_layers=2, bidirectional=True)
+        self.segLSTM = nn.LSTM(74, 32, num_layers=2,
+                               bidirectional=True, batch_first=True)
         # Wrap the Convolutional Blocks
         self.conv = nn.Sequential(*conv_layers)
         self.dp = nn.Dropout(p=0.3)
